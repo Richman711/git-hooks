e4e810dfc8ddb000072a986dc2a2bbd6fd0c3b0d
richard orman <Test@test.com>
2023-02-11 12:44:09 +0900

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module main.com
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func World(i int) string {
+	if i == 5 {
+		return "World!"
+	} else {
+		return "New World!"
+	}
+}
+func main() {
+	fmt.Print("Hello ")
+	suffix := World(2)
+	fmt.Println(suffix)
+}
